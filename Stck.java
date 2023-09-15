35169febc9c6af0aded395d4fe825e449fd44f55
23naman <118149485+23naman@users.noreply.github.com>
2023-09-15 18:49:10 +0530

    Update Stck.java

--- Stck.java
+++ Stck.java
@@ -1,67 +1,63 @@
-class stck{
+class stck {
 
-int arr[]= new int[5];
-int top;
-int r;
-void push(int n){
+  int arr[] = new int[5];
+  int top;
+  int r;
+  void push(int n) {
 
-if(top<4)
-{
-top++;
-r=n;
-arr[top]=r;
-}
-else{
-System.out.println("Stack Overflow");
+    if (top < 4) {
+      top++;
+      r = n;
+      arr[top] = r;
+    } else {
+      System.out.println("Stack Overflow");
 
-}
+    }
 
-}
-void pop(int n){
-if(top==-1)
-{System.out.println("Stact underflew");}
-else
-{r=n;
-arr[top--]=r;}
+  }
+  void pop(int n) {
+    if (top == -1) {
+      System.out.println("Stact underflew");
+    } else {
+      r = n;
+      arr[top--] = r;
+    }
 
-}
-stck(){
-top=-1;
-}
+  }
+  stck() {
+    top = -1;
+  }
 
-void display(){
-System.out.println("The element at position "+ top +" is " + arr[top]);
-}
+  void display() {
+    System.out.println("The element at position " + top + " is " + arr[top]);
+  }
 
+  public static void main(String args[]) {
+    stck X = new stck();
+    X.push(8);
+    X.display();
+    X.push(2);
+    X.display();
+    X.push(65);
+    X.display();
+    X.push(7);
+    X.display();
+    X.push(89);
+    X.display();
 
+    X.push(9);
+    X.display();
 
+    X.pop(10);
+    X.display();
+    X.pop(10);
+    X.display();
+    X.pop(10);
+    X.display();
+    X.pop(10);
+    X.display();
+    X.pop(10);
+    X.display();
 
-public static void main(String args[]){
-stck X = new stck();
-X.push(8);
-X.display();
-X.push(2);
-X.display();
-X.push(65);
-X.display();
-X.push(7);
-X.display();
-X.push(89);
-X.display();
-
-X.push(9);
-X.display();
-
-X.pop(10);
-X.display();
-X.pop(10);
-X.display();
-X.pop(10);
-X.display();
-X.pop(10);
-X.display();
-X.pop(10);
-X.display();
-
-}
+  }
 }
